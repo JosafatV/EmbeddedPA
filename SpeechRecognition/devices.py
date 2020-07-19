0f331ae1d8dd671f673627e86d8889acafd23a3b
JosafatV <josav09@gmail.com>
2020-07-19 16:45:59 -0600

    recording in x86, run from console

---
+++ SpeechRecognition/devices.py
@@ -0,0 +1,12 @@
+"""PyAudio Example: Play a WAVE file."""
+
+import speech_recognition as sr
+import pyaudio
+import wave
+
+print("----------------------record device list---------------------")
+info = audio.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+        if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
