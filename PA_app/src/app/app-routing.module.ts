8c3c00d58de1618bd7a1ab062c0873d5cda9eeb2
Fera95 <ferarios95@gmail.com>
2020-07-23 18:42:26 -0600

    Se hacen cambios para la nueva app

--- PA_app/src/app/app-routing.module.ts
+++ PA_app/src/app/app-routing.module.ts
@@ -14,31 +14,13 @@ const routes: Routes = [
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
   {
-    path: 'admin',
-    loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule)
-  },
-  {
-    path: 'casa-details',
-    loadChildren: () => import('./pages/casa-details/casa-details.module').then( m => m.CasaDetailsPageModule)
-  },
-  {
-    path: 'casa-details/:id',
-    loadChildren: () => import('./pages/casa-details/casa-details.module').then( m => m.CasaDetailsPageModule)
-  },
-  {
     path: 'files',
     loadChildren: () => import('./files/files.module').then( m => m.FilesPageModule)
   },
   {
     path: 'cloud-list',
     loadChildren: () => import('./cloud-list/cloud-list.module').then( m => m.CloudListPageModule)
-  },
-  {
-    path: 'lights',
-    loadChildren: () => import('./lights/lights.module').then( m => m.LightsPageModule)
   }
-
-
 
 ];
 
