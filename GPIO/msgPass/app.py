6770f8e7af30408867a6e9ac3ffd6832228160d5
JosafatV <josav09@gmail.com>
2020-08-02 23:39:32 -0600

    adding simple way of sending messages

---
+++ GPIO/msgPass/app.py
@@ -0,0 +1,24 @@
+import time
+from fileManager import readFile, writeFile
+
+def choose_data (select):
+    if(select == 0):
+        return "1:0:0"
+    if(select == 1):
+        return "0:1:0"
+    if(select == 2):
+        return "0:0:1"
+
+select = 0
+while(True):
+    
+    time.sleep(2.5)
+    read_data = readFile("btn_status.txt")
+    print(read_data)
+    
+    time.sleep(2.5)
+    data = choose_data(select)
+    writeFile("led_status.txt", data)
+    print(data)
+    select = (select+1)%3
+    
